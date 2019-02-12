b4d2277f9f6a4d736efba34a56f2583cf1278b93
Martin Schwarzer <polyluxus@gmail.com>
2019-02-12 14:18:18 +0100

    Bugfix: (style) include ~/.config/ into search locations, minor code rewrite for style

--- runxtb.sh
+++ runxtb.sh
@@ -129,22 +129,23 @@ expand_tilde_path ()
 
 get_bindir ()
 {
-  local resolve_file="$1" description="$2" link_target directory_name resolve_dir_name
+  # Resolves the absolute location of parameter and returns it
+  # partially taken from https://stackoverflow.com/a/246128/3180795
+  local resolve_file="$1" description="$2" 
+  local link_target directory_name resolve_dir_name
   debug "Getting directory for '$resolve_file'."
+  resolve_file=$( expand_tilde_path "$resolve_file" )
 
-  resolve_file=$(expand_tilde_path "$resolve_file")
-
-  # Taken in part from https://stackoverflow.com/a/246128/3180795
   # resolve $resolve_file until it is no longer a symlink
-  while [ -h "$resolve_file" ]; do 
-    link_target="$(readlink "$resolve_file")"
+  while [[ -h "$resolve_file" ]] ; do 
+    link_target="$( readlink "$resolve_file" )"
     if [[ $link_target == /* ]]; then
       debug "File '$resolve_file' is an absolute symlink to '$link_target'"
       resolve_file="$link_target"
     else
       directory_name="$( dirname "$resolve_file" )" 
       debug "File '$resolve_file' is a relative symlink to '$link_target' (relative to '$directory_name')"
-      #  If $SOURCE was a relative symlink, we need to resolve 
+      #  If $resolve_file was a relative symlink, we need to resolve 
       #+ it relative to the path where the symlink file was located
       resolve_file="$directory_name/$link_target"
     fi
@@ -152,7 +153,7 @@ get_bindir ()
   debug "File is '$resolve_file'" 
   resolve_dir_name="$( dirname "$resolve_file")"
   directory_name="$( cd -P "$( dirname "$resolve_file" )" && pwd )"
-  if [ "$directory_name" != "$resolve_dir_name" ]; then
+  if [[ "$directory_name" != "$resolve_dir_name" ]] ; then
     debug "$description '$directory_name' resolves to '$directory_name'"
   fi
   debug "$description is '$directory_name'"
@@ -314,14 +315,14 @@ test_rc_file ()
 get_rc ()
 {
   local test_runxtbrc_dir test_runxtbrc_loc return_runxtbrc_loc
-  while [[ ! -z $1 ]] ; do
+  while [[ -n $1 ]] ; do
     test_runxtbrc_dir="$1"
     shift
-    if test_runxtbrc_loc="$(test_rc_file "$test_runxtbrc_dir/.runxtbrc")" ; then
+    if test_runxtbrc_loc="$( test_rc_file "$test_runxtbrc_dir/.runxtbrc" )" ; then
       return_runxtbrc_loc="$test_runxtbrc_loc" 
       debug "   (found) return_runxtbrc_loc=$return_runxtbrc_loc"
       continue
-    elif test_runxtbrc_loc="$(test_rc_file "$test_runxtbrc_dir/runxtb.rc")" ; then 
+    elif test_runxtbrc_loc="$( test_rc_file "$test_runxtbrc_dir/runxtb.rc" )" ; then 
       return_runxtbrc_loc="$test_runxtbrc_loc"
       debug "   (found) return_runxtbrc_loc=$return_runxtbrc_loc"
       continue
@@ -525,14 +526,16 @@ declare -a load_modules
 stay_quiet=0
 ignore_empty_commandline=false
 
-scriptpath="$(get_bindir "$0" "Directory of runxtb")"
-runxtbrc_loc="$(get_rc "$scriptpath" "/home/$USER" "$PWD")"
+scriptpath="$( get_bindir "$0" "Directory of runxtb" )"
+runxtbrc_loc="$(get_rc "$scriptpath" "/home/$USER" "/home/$USER/.config/" "$PWD")"
 debug "runxtbrc_loc=$runxtbrc_loc"
 
-if [[ ! -z $runxtbrc_loc ]] ; then
+if [[ -n $runxtbrc_loc ]] ; then
   # shellcheck source=/home/te768755/devel/runxtb.bash/runxtb.rc
   . "$runxtbrc_loc"
   message "Configuration file '$runxtbrc_loc' applied."
+else
+  debug "No configuration file found."
 fi
 
 OPTIND=1
